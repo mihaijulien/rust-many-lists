d4bd0e91b8ff6a9921866aaa53ffb51a7bf4dc7f
mihaijulien <mihaijulien@gmail.com>
2025-09-10 21:09:56 +0300

    implement peeking

--- src/second.rs
+++ src/second.rs
@@ -30,6 +30,18 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -41,3 +53,16 @@ impl<T> Drop for List<T> {
     }
 }
 
+mod test{
+    use super::List;
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+    }
+}
